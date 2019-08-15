bb4ca1dfc40109e74451aae04ce38958a13cd8ad
Timothy Cho <tcho9522@gmail.com>
2019-08-15 11:20:04 -0500

    first commit

---
+++ src/main/java/com/trilogyed/cloudconfigservice/CloudConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.cloudconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServiceApplication.class, args);
+	}
+
+}
